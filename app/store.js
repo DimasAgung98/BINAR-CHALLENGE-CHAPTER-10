22a3d2b3a3df9f8a9347ddec5388976c0a77ba8f
pashateuku <thariqteuku.soc@gmail.com>
2022-08-19 23:37:05 +0700

    add gameHistory Action, Reducer, and Store

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 // Import Redux
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from '../features/authentication/authenticationSlice';
+import gameHistoryReducer from '../features/gameHistory/gameHistorySlice';
 // Import redux-persist
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -12,10 +13,12 @@ const persistConfig = {
 }
 
 const persistedAuthReducer = persistReducer(persistConfig, authenticationReducer)
+const persistedGameHistoryReducer = persistReducer(persistConfig, gameHistoryReducer)
 
 const store = configureStore({
   reducer: {
-    authentication: persistedAuthReducer
+    authentication: persistedAuthReducer,
+    gameHistory: persistedGameHistoryReducer
   },
 });
 
