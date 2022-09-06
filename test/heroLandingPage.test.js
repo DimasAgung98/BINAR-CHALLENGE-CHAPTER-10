765695730626d584e87425e7cbb20381ead29405
dimas agung <dimasraden0798@gmail.com>
2022-09-07 01:55:28 +0800

    testing error

--- test/heroLandingPage.test.js
+++ test/heroLandingPage.test.js
@@ -1,20 +1,64 @@
+import renderer from 'react-test-renderer';
+import { createRef } from 'react';
+import * as TestUtils from 'react-dom/test-utils';
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom/client';
 import HeroLandingPage from '../components/landingPage/HeroLandingPage';
+import { act } from 'react-dom/test-utils';
+import Sidebar from '../components/HomePage/Sidebar';
+import gambar from '../public/apex.jpg';
+import Image from 'next/image';
 
 describe('Testing Component HeroLandingPage', () => {
-    test('renders landingPage', () => {
-        render(<HeroLandingPage />);
-        const Title = screen.getByText(/play various games/i);
-        expect(Title).toBeInTheDocument();
+    test('Add Text HerolandingPage', () => {
+        const component = renderer.create(
+            <>
+                <div>
+                    <h1>Team One</h1>
+                    <p>lorem ipsum</p>
+                </div>
+            </>
+        )
+        let page = component.toJSON();
+        expect(page).toMatchSnapshot();
+    });
+
+    test('Add Form HerolandingPage', () => {
+        const component = renderer.create(
+            <>
+                <div>
+                    <h1>Team One</h1>
+                </div>
+                <div>
+                    <form>Team :</form>
+                    <input>One</input>
+                </div>
+            </>
+        )
+        let page = component.toJSON();
+        expect(page).toMatchSnapshot();
+    });
+
+    test('Add Image HerolandingPage', () => {
+        const component = renderer.create(
+            <>
+                <div>
+                    <Image src={gambar} alt='image'></Image>
+                </div>
+            </>
+        )
+        let page = component.toJSON();
+        expect(page).toMatchSnapshot();
+    });
+
+    let container;
+    test('Add Componment sidebar HerolandingPage', () => {
+        act(() => {
+            ReactDOM.render(<Sidebar />);
+        })
+    });
+
+    test('Expect Value HerolandingPage', () => {
+        expect(HeroLandingPage()).toBe('teamone');
     });
 })
-
-const title1 = "Play various games from Team One Project and enjoy the fun."
-
-describe("check herolandingpage", () => {
-    test("check title", async () => {
-        const title = await HeroLandingPage(title1)
-        expect(title.title1 === "Play various games from Team One Project and enjoy the fun.").toBeTruthy();
-    });
-});
-
